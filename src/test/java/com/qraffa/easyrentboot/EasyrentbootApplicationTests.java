846d3c58091e66d21e83d5ab9879e737067e0fb7
Qraffa <qraffa@163.com>
2020-05-08 13:49:52 +0800

    添加:配置文件加密

--- src/test/java/com/qraffa/easyrentboot/EasyrentbootApplicationTests.java
+++ src/test/java/com/qraffa/easyrentboot/EasyrentbootApplicationTests.java
@@ -3,6 +3,7 @@ package com.qraffa.easyrentboot;
 import com.louislivi.fastdep.shirojwt.jwt.JwtUtil;
 import com.qraffa.easyrentboot.model.exception.ExceptionModel;
 import com.qraffa.easyrentboot.service.UserService;
+import org.jasypt.encryption.StringEncryptor;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,15 +13,9 @@ import javax.sql.DataSource;
 @SpringBootTest
 class EasyrentbootApplicationTests {
 
-    @Autowired
-    UserService userService;
-
-    @Autowired
-    JwtUtil jwtUtil;
-
     @Test
     void contextLoads() {
-        System.out.println(jwtUtil.getUserId("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxIiwiZXhwIjoxNTg2NjI0NDc2fQ.2tKW2c8A7urYMLERrzqwKUUcQVGIKXSbMO8UtkteGwI"));
+
     }
 
 }
